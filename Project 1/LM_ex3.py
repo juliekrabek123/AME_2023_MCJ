8fed15ade5b84841a4ff855ab196150402329c05
juliekrabek123 <hjq973@alumni.ku.dk>
2023-09-27 18:01:08 +0200

    julie

--- Project 1/LM_ex3.py
+++ Project 1/LM_ex3.py
@@ -1,10 +1,11 @@
 import numpy as np
 from numpy import linalg as la
+import scipy.stats 
 from tabulate import tabulate
 
 
 def estimate( 
-        y: np.ndarray, x: np.ndarray, transform='', T:int=None
+        y: np.ndarray, x: np.ndarray, transform='', T:int=None, robust = False, sigma2_c:int = 0, sigma2_u:int = 0
     ) -> list:
     """Uses the provided estimator (mostly OLS for now, and therefore we do 
     not need to provide the estimator) to perform a regression of y on x, 
@@ -35,11 +36,41 @@ def estimate(
     SST = (y - np.mean(y)).T@(y - np.mean(y))  # Total sum of squares
     R2 = 1 - SSR/SST
 
-    sigma2, cov, se = variance(transform, SSR, x, T)
+    sigma2, cov, se = variance(transform, SSR, x, T, robust, residual, sigma2_c, sigma2_u)
     t_values = b_hat/se
+
+    p_values = []
+    for t_val in t_values:
+        p_value = 2 * (scipy.stats.t.sf(np.abs(t_val), df=(x.shape[0] - x.shape[1])))
+        p_values.append(p_value)
+    p_values = np.array(p_values)
+
+    # Map p-values to asterisk symbols
+    def map_p_value_to_asterisks(p_value):
+        if p_value <= 0.001:
+            return '***'
+        elif p_value <= 0.01:
+            return '**'
+        elif p_value <= 0.05:
+            return '*'
+        elif p_value <= 0.1:
+            return '.'
+        else:
+            return ''
     
-    names = ['b_hat', 'se', 'sigma2', 't_values', 'R2', 'cov']
-    results = [b_hat, se, sigma2, t_values, R2, cov]
+    asterisk_symbols = []
+    for p_value in p_values:
+        asterisk = map_p_value_to_asterisks(p_value[0])  # Assuming p_value is a 1D array
+        asterisk_symbols.append([asterisk])
+    asterisk_symbols = np.array(asterisk_symbols)
+
+    p_values_float = [float(p) for p in p_values]
+
+    p_values_ast = [f'{p:.4f} ({asterisk})' for p, asterisk in zip(p_values_float, asterisk_symbols)]
+
+    names = ['b_hat', 'se', 'sigma2', 't_values', 'p_values_ast', 'R2', 'cov', 'ast']
+    results = [b_hat, se, sigma2, t_values, p_values_ast, R2, cov, asterisk_symbols]
+
     return dict(zip(names, results))
 
     
@@ -59,7 +90,11 @@ def variance(
         transform: str, 
         SSR: float, 
         x: np.ndarray, 
-        T: int
+        T: int,
+        robust: bool, 
+        residual: np.ndarray,
+        sigma2_c: float,
+        sigma2_u: float
     ) -> tuple:
     """Calculates the covariance and standard errors from the OLS
     estimation.
@@ -103,14 +138,39 @@ def variance(
         raise Exception('Invalid transform provided.')
     
     cov = sigma2*la.inv(x.T@x)
+
+    if robust is True:
+        if not transform.lower() == 're':
+            cov_v_out = 0
+            for i in range(int(N)):
+                idx_i = slice(i*T, (i+1)*T) # index values for individual i 
+                xi = x[idx_i]
+                res_outer = residual[idx_i]@residual[idx_i].T
+                cov_v_out += xi.T@res_outer@xi
+            cov_v = la.inv(x.T@x) @ (cov_v_out) @ la.inv(x.T@x)
+
+        if transform.lower() == 're':
+            res_split = np.split(residual,N)
+            omega = sigma2_u*np.eye(T) + sigma2_c*np.ones((T, T))
+            x_split = np.split(x,N)
+            A_out = 0
+            B_out = 0
+            for i in range(int(N)):
+                A_out += x_split[i].T@la.inv(omega)@x_split[i]
+                B_out += x_split[i].T@la.inv(omega)@res_split[i]@res_split[i].T@la.inv(omega)@x_split[i]
+            cov_v = la.inv(A_out) @ B_out @ la.inv(A_out)      
+
+        cov = cov_v.copy()
+
     se = np.sqrt(cov.diagonal()).reshape(-1, 1)
+
     return sigma2, cov, se
 
 
 def print_table(
         labels: tuple,
         results: dict,
-        headers=["", "Beta", "Se", "t-values"],
+        headers=["", "Beta", "Se", "t-values", "p-values"],
         title="Results",
         _lambda:float=None,
         **kwargs
@@ -143,7 +203,8 @@ def print_table(
             name, 
             results.get('b_hat')[i], 
             results.get('se')[i], 
-            results.get('t_values')[i]
+            results.get('t_values')[i],
+            results.get('p_values_ast')[i]
         ]
         table.append(row)
     
